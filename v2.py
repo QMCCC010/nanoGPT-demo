6f42184900528596ba24b7f62efa8d39ed5b6c52
QMCCC_OMEN <2868916930@qq.com>
2026-05-18 10:13:56 +0800

    bigram version2.

--- v2.py
+++ v2.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch.nn.functional import dropout
 
 # 超参数
-batch_size = 32
-block_size = 8
-max_iters = 10000
+batch_size = 64
+block_size = 256
+max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200 # 一次用200个loss的平均值来评估
-n_embd = 32 # 词嵌入大小（即每个token转化为向量的大小）
+n_embd = 384# 词嵌入大小（即每个token转化为向量的大小）
+n_head = 6 # 自注意力头数
+n_transformer = 6 # transformer块的数量
+dropout = 0.2 # dropout的概率
 # ---------------------------------------------------------
 
 torch.manual_seed(42)
@@ -75,24 +79,69 @@ class Head(nn.Module):
 
     def __init__(self, head_size):
         super().__init__()
+        self.head_size = head_size
         self.key = nn.Linear(n_embd, head_size, bias=False)
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x) # (B, T, C), C = head_size
         q = self.query(x) # (B, T, C), C = head_size
 
-        weight = q @ k.transpose(-2, -1) * C**-5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        weight = q @ k.transpose(-2, -1) * self.head_size**-5 # (B, T, C) @ (B, C, T) -> (B, T, T)
         weight = weight.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         weight = F.softmax(weight, dim=-1)
+        weight = self.dropout(weight)
         v = self.value(x) # (B, T, C), C = head_size
         out = weight @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
         return out
 
 
+# 多头注意力
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd) # projection 投影
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
+
+# 前馈层
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self,x):
+        return self.net(x)
+
+# TransformerBlock
+class TransformerBlock(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 # 二元语言模型
 class BigramLanguageModel(nn.Module):
 
@@ -100,7 +149,10 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # 词嵌入表
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # 位置嵌入表
-        self.sa_head = Head(n_embd)
+        self.TransformerBlock = nn.Sequential(
+            *[TransformerBlock(n_embd, n_head) for _ in range (n_transformer)]
+        )
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -108,8 +160,9 @@ class BigramLanguageModel(nn.Module):
 
         tok_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
-        x = tok_emb + pos_emb
-        x = self.sa_head(x)
+        x = tok_emb + pos_emb # (B, T, C)
+        x = self.TransformerBlock(x) # (B, T, C)
+        x = self.ln_f(x) # (B, T, C)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
